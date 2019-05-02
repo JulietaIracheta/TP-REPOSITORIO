6ab9e4736dbef5924ed22ffab18653f5c8fcd005
Julieta <julieta.iracheta@hotmail.com>
2019-05-02 16:18:53 -0300

    creada clase continente

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Continente.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Continente.java
@@ -1,5 +1,29 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Continente {
 
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Integer nombre;
+	
+	//Getter and Setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Integer getNombre() {
+		return nombre;
+	}
+	public void setNombre(Integer nombre) {
+		this.nombre = nombre;
+	}
 }
