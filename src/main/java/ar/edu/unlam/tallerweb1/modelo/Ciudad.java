5091630eed7aee3970b5eb06355a23a5e8925e3e
Julieta <julieta.iracheta@hotmail.com>
2019-05-02 16:42:19 -0300

    realizado

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Ciudad.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Ciudad.java
@@ -1,5 +1,61 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.Cascade;
+
+
+
+
+@Entity
 public class Ciudad {
 
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre; 
+	private String ubicaciongeo;
+	
+	@ManyToOne
+	@Cascade(value= CascadeType.ALL)
+	private Pais pais;
+
+	
+	//Getters and Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getUbicaciongeo() {
+		return ubicaciongeo;
+	}
+
+	public void setUbicaciongeo(String ubicaciongeo) {
+		this.ubicaciongeo = ubicaciongeo;
+	}
+
+	public Pais getPais() {
+		return pais;
+	}
+
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
 }
