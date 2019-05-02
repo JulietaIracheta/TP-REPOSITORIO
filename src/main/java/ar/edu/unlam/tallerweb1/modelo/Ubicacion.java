ed6a347ebc8891fa7497bd2a6e7ade614905d9d0
Julieta <julieta.iracheta@hotmail.com>
2019-05-02 16:57:21 -0300

    clase ubicacion

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Ubicacion.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Ubicacion.java
@@ -4,14 +4,51 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Ubicacion {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;
+	private Double latitud;
+	private Double longitud;
 	
+	@OneToOne
+	private Ubicacion ubicacion;
+
 	
+	//Getters and setters
 	
-	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getLatitud() {
+		return latitud;
+	}
+
+	public void setLatitud(Double latitud) {
+		this.latitud = latitud;
+	}
+
+	public Double getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(Double longitud) {
+		this.longitud = longitud;
+	}
+
+	public Ubicacion getUbicacion() {
+		return ubicacion;
+	}
+
+	public void setUbicacion(Ubicacion ubicacion) {
+		this.ubicacion = ubicacion;
+	}
 }
