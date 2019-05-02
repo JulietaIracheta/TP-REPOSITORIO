4398dc4625a858fd33b00cfc3d2f0cd495d11a30
Julieta <julieta.iracheta@hotmail.com>
2019-05-02 16:55:59 -0300

    clase ubicacion creada

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Ubicacion.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Ubicacion.java
@@ -1,5 +1,17 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
-public class Ubicacion {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Ubicacion {
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	
+	
+	
 }
